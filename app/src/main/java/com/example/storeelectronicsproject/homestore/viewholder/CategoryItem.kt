60ab0537b3c8c79f2d6147c732b5ea6172767196
VIP <belevitin2010@mail.ru>
2022-11-11 21:55:09 +0300

    Created a color change when clicking on the category item

--- app/src/main/java/com/example/storeelectronicsproject/homestore/viewholder/CategoryItem.kt
+++ app/src/main/java/com/example/storeelectronicsproject/homestore/viewholder/CategoryItem.kt
@@ -12,21 +12,55 @@ class CategoryItem(
     private val categoryData: CategoryData
 ) : AbstractBindingItem<RecyclerCategoryItemHomeStoreBinding>() {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun bindView(binding: RecyclerCategoryItemHomeStoreBinding, payloads: List<Any>) {
         super.bindView(binding, payloads)
 
         binding.textNameCategoryItem.text = categoryData.name
 
         if (categoryData.isSelected) {
+            binding.imageCategoryBackgroundItem.setImageDrawable(
+                binding.root.resources.getDrawable(
+                    R.drawable.style_image_category_item,
+                    binding.root.context.theme
+                )
+            )
         }
-        binding.imageCategoryItem.setImageDrawable(binding.root.resources.getDrawable(categoryData.image, binding.root.context.theme))
+
+        binding.backgroundCategoryItem.setOnClickListener {
+            if (categoryData.isSelected) {
+                categoryData.isSelected = false
+                binding.imageCategoryBackgroundItem.setImageDrawable(
+                    binding.root.resources.getDrawable(
+                        R.drawable.style_image_category_false_item,
+                        binding.root.context.theme
+                    )
+                )
+            } else {
+                categoryData.isSelected = true
+                binding.imageCategoryBackgroundItem.setImageDrawable(
+                    binding.root.resources.getDrawable(
+                        R.drawable.style_image_category_item,
+                        binding.root.context.theme
+                    )
+                )
+            }
+        }
+
+        binding.imageCategoryItem.setImageDrawable(
+            binding.root.resources.getDrawable(
+                categoryData.image,
+                binding.root.context.theme
+            )
+        )
 
     }
 
     override fun createBinding(
         inflater: LayoutInflater,
         parent: ViewGroup?
-    ): RecyclerCategoryItemHomeStoreBinding = RecyclerCategoryItemHomeStoreBinding.inflate(inflater, parent, false)
+    ): RecyclerCategoryItemHomeStoreBinding =
+        RecyclerCategoryItemHomeStoreBinding.inflate(inflater, parent, false)
 
     override val type: Int = R.id.background_category_item
 }
